e9df23a060813970867a786e209b47050ef2e37f
Alberto Ramos <alberto.ramos@desy.de>
2020-07-14 19:41:09 +0100

    Added license

--- src/ADerrorsIO.jl
+++ src/ADerrorsIO.jl
@@ -1,3 +1,13 @@
+###
+### "THE BEER-WARE LICENSE":
+### Alberto Ramos wrote this file. As long as you retain this 
+### notice you can do whatever you want with this stuff. If we meet some 
+### day, and you think this stuff is worth it, you can buy me a beer in 
+### return. <alberto.ramos@cern.ch>
+###
+### file:    ADerrorsIO.jl
+### created: Tue Jul 1 19:40:39 2020
+###                               
 
 function find_mcid(a::uwreal, mcid::Int64)
 
